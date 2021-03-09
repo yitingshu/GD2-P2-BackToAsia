8a6f4cced3b0f6f43a1062215bad03b5de44f2b0
yitingshu <80033665+yitingshu@users.noreply.github.com>
2021-03-09 09:55:33 +0800

    Add files via upload

---
+++ book/script.js
@@ -0,0 +1,29 @@
+console.log("book");
+
+
+window.onload = function(){
+	$(".first img").draggable();
+	$(".second img").draggable();
+	$(".third img").draggable();
+	$(".fourth img").draggable();
+	$(".fifth img").draggable();
+	$(".sixth img").draggable();
+	$(".seventh img").draggable();
+	$(".eight img").draggable();
+	$(".nine img").draggable();
+	$(".tenth img").draggable();
+	$(".eleventh img").draggable();
+	$(".twelfth img").draggable();
+	$(".thirtieth img").draggable();
+	$(".fourtieth img").draggable();
+	$(".fiftieth img").draggable();
+	$(".sixteenth img").draggable();
+	$(".seventeenth img").draggable();
+	$(".eighteenth img").draggable();
+	$(".nineteenth img").draggable();
+	$(".twentieth img").draggable();
+	$(".twentyfirst img").draggable();
+	$(".twentysecond img").draggable();
+	$(".twentythird img").draggable();
+
+}
